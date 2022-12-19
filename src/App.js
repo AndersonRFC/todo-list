cefcdd615800e8856edbc717646a3fa592bd1673
AndersonRFC <anderson.informatica2016@gmail.com>
2022-12-19 20:59:41 -0300

    Antes da criação da home.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./componentes/Form";
+import TodoItem from "./componentes/TodoItem";
+import { Container } from "@mui/system";
+import { List } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xs" style={{ marginTop: "1em" }}>
+      <Form />
+      <List sx={{ bgcolor: "background.paper", marginTop: "1em" }}>
+      <TodoItem />
+      </List>
+    </Container>
   );
 }
 
